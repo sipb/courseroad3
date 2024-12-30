13f7f84ab826e1a27ecb538d3ba1c32c69048a7f
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-12-29 23:44:39 -0600

    add user class year

---
+++ src/routes/api/people/[kerb].ts
@@ -0,0 +1,36 @@
+import type { APIEvent } from "@solidjs/start/server";
+
+export async function GET({ params }: APIEvent) {
+  const kerb = params.kerb;
+  const headers = new Headers();
+
+  headers.append("client_id", process.env.PEOPLEAPI_ID);
+  headers.append("client_secret", process.env.PEOPLEAPI_SECRET);
+
+  const init = { method: "GET", headers };
+  const response = await fetch(
+    "https://mit-people-v3.cloudhub.io/people/v3/people/{kerberosId}",
+    init
+  );
+  console.log(`response status is ${response.status}`);
+
+  if (response.status !== 200) {
+    return new Response("Could not get user data", { status: 400 });
+  }
+
+  const data = await response.json();
+  console.log(data);
+
+  if (data.item.affiliations[0].type !== "student") {
+    return new Response("User is not a student", { status: 400 });
+  }
+
+  const year: string = data.item.affiliations[0].classYear;
+
+  if (year === "G") {
+    return new Response("User is a graduate student", { status: 400 });
+  }
+
+  const yearNumber = parseInt(year, 10) - 1;
+  return new Response(yearNumber.toString(), { status: 200 });
+}
---
+++ src/routes/api/people/index.ts
@@ -0,0 +1,3 @@
+export async function GET() {
+  return new Response("Kerb was not specified", { status: 400 });
+}
