3f61ce09861a1e520590a6bccc0f1b3784fcbe2f
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-09-14 17:51:57 -0400

    bump everything

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
 
-import devtools from "solid-devtools/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	vite: {
-		plugins: [tsconfigPaths({ root: "./" }), devtools({})],
+		plugins: [tsconfigPaths({ root: "./" })],
 	},
 	server: {
 		compatibilityDate: "2024-12-30",
--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,6 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 
-import "solid-devtools";
 import "./index.css";
 
 export default function App() {
